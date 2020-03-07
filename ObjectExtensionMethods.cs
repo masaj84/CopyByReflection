f10d76dec83d69cca946524f36d1980ceb275c28
Michal <michal.maka@gmail.com>
2020-03-07 12:24:18 +0100

    Add files via upload

---
+++ ObjectExtensionMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    public static class ObjectExtensionMethods
+    {
+        public static void GetCopyByReflection(this object source, object destination)
+        {
+            var srcProperties = source.GetType().GetProperties();
+            var dstProperties = destination.GetType().GetProperties();
+                                   
+            foreach (var destinationItem in dstProperties)
+            {
+                foreach (var srcItem in srcProperties)
+                {
+                    if (srcItem.Name == destinationItem.Name && srcItem.PropertyType == destinationItem.PropertyType)
+                    {
+                        destinationItem.SetValue(destination, srcItem.GetValue(source));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCat = new cat();
+            //myCat.name = "Jojo";
+            //myCat.age = 12;
+            //myCat.price = 100;
+            //myCat.date = DateTime.Now;
+            //myCat.catRace = race.Asian_cat;
+            //myCat.catHome = new home {
+            //    postcode = "00-002",
+            //    town = "Warsaw"
+            //};       
+
+            var newCat = new cat();
+            myCat.GetCopyByReflection(newCat);
+
+            Console.WriteLine(newCat.age);
+            Console.WriteLine(newCat.catRace);
+            Console.WriteLine(newCat.catHome.town);
+            Console.ReadLine();
+        }
+
+        public class cat {
+
+            public cat()
+            {
+                name = "Jojo";
+                age = 15;
+                price = 100;
+                date = DateTime.Now;
+                catRace = race.Asian_cat;
+                catHome = new home
+                {
+                    postcode = "00-002",
+                    town = "Warsaw"
+                };
+            }
+            public string name { get; set; }
+            public int age { get; set; }
+            public decimal price { get; set; }
+            public DateTime date { get; set; }
+            public race catRace {get; set;}
+            public home catHome { get; set; }
+        }
+
+        public class home { 
+            public string town { get; set; }
+            public string postcode { get; set; }
+        }
+
+        public enum race
+        {
+            Abyssinian,
+            Aegean,
+            Asian_cat
+        }
+    }
+}
